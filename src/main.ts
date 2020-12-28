6badcf06291a8b6333afd264a87c60dc835f0ac3
Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>
2020-12-28 19:47:48 +0800

    chore: remove @sentry/tracing and code

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,6 @@ import logger from 'koa-logger';
 import json from 'koa-json';
 import bodyParser from 'koa-bodyparser';
 import * as Sentry from '@sentry/node';
-import {
-  extractTraceparentData,
-  stripUrlQueryAndFragment
-} from '@sentry/tracing';
-import * as domain from 'domain';
 
 const app: Koa = new Koa();
 const router: Router = new Router();
@@ -17,59 +12,6 @@ const port = process.env.PORT || 3000;
 Sentry.init({
   tracesSampleRate: 1.0
 });
-
-const requestHandler = (ctx, next) => {
-  return new Promise<void>((resolve, _) => {
-    const local = domain.create();
-    local.add(ctx);
-    local.on('error', err => {
-      ctx.status = err.status || 500;
-      ctx.body = err.message;
-      ctx.app.emit('error', err, ctx);
-    });
-    local.run(async () => {
-      Sentry.getCurrentHub().configureScope(scope =>
-        scope.addEventProcessor(event =>
-          Sentry.Handlers.parseRequest(event, ctx.request, {user: false})
-        )
-      );
-      await next();
-      resolve();
-    });
-  });
-};
-
-// this tracing middleware creates a transaction per request
-const tracingMiddleWare = async (ctx, next) => {
-  const reqMethod = (ctx.method || '').toUpperCase();
-  const reqUrl = ctx.url && stripUrlQueryAndFragment(ctx.url);
-
-  // connect to trace of upstream app
-  let traceparentData;
-  if (ctx.request.get('sentry-trace')) {
-    traceparentData = extractTraceparentData(ctx.request.get('sentry-trace'));
-  }
-
-  const transaction = Sentry.startTransaction({
-    name: `${reqMethod} ${reqUrl}`,
-    op: 'http.server',
-    ...traceparentData
-  });
-
-  ctx.__sentry_transaction = transaction;
-  await next();
-
-  // if using koa router, a nicer way to capture transaction using the matched route
-  if (ctx._matchedRoute) {
-    const mountPath = ctx.mountPath || '';
-    transaction.setName(`${reqMethod} ${mountPath}${ctx._matchedRoute}`);
-  }
-  transaction.setHttpStatus(ctx.status);
-  transaction.finish();
-};
-
-app.use(requestHandler);
-app.use(tracingMiddleWare);
 
 app.on('error', (err, ctx) => {
   Sentry.withScope(function (scope) {
